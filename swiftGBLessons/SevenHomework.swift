eac171c60974f029d005484f93e7210dd0684c7a
grimurrash <rashit.sabirov1999@gmail.com>
2022-03-04 01:55:27 -0800

    Seven Homework

---
+++ swiftGBLessons/SevenHomework.swift
@@ -0,0 +1,10 @@
+//
+//  SevenHomework.swift
+//  swiftGBLessons
+//
+//  Created by Rashit Sabirov on 04.03.2022.
+//
+
+import Foundation
+
+// Обработка ошибок реализовано в прошлых домашних заданиях
